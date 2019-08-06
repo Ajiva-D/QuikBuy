7713ce8f7ae6f54377e9d3a21437d1658c2a5c33
Ajiva-D <davidenochaji@gmail.com>
2019-08-06 14:05:44 +0100

    updates to hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,54 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
 // import herobg from './../images/ladybg.jpg'
 // import vans from './../images/vans.jpg'
 // import jean from './../images/jeanjacket.jpg'
 // import redbag from './../images/redbag.jpg'
 // import sneakers from './../images/hotsneakers.jpg'
-import Rotate from 'react-reveal/Rotate';
-import Slide from 'react-reveal/Slide';
+import { Image, Video, Transformation, CloudinaryContext } from "cloudinary-react";
+import bg from "./../images/ecom/Dp3Pl_JWkAEkqJL.jpg";
+import one from "./../images/fs-1.jpg";
+import two from "./../images/fs-2.jpg";
+import three from "./../images/fs-3.jpg";
+import four from "./../images/fs-4.jpg";
+import Rotate from "react-reveal/Rotate";
+import Slide from "react-reveal/Slide";
 
-const Hero = () =>{
-	return(
+const Hero = () => {
+	return (
 		<div className="hero">
 			<div className="empty">
 				<Slide left>
-				<h1>SHOP SAVE</h1>	
-				<h1 id="h1-2">CHECKOUT</h1>
+					<h1>SHOP SAVE</h1>
+					<h1 id="h1-2">CHECKOUT</h1>
 				</Slide>
-				
-			<div className="grid-con">
-					<img className="img1" src="https://res.cloudinary.com/ajiva/image/upload/v1563881533/quikbuy/vans.jpg" alt="vans"/>
-					<img className="img2" src="https://res.cloudinary.com/ajiva/image/upload/v1563881526/quikbuy/jeanjacket.jpg" alt="jeanjacket"/>
-					<img className="img3" src="https://res.cloudinary.com/ajiva/image/upload/v1563881512/quikbuy/redbag.jpg" alt="red bag"/>
-					<img className="img4" src="https://res.cloudinary.com/ajiva/image/upload/v1563881524/quikbuy/hotsneakers.jpg" alt="sneakers"/>
-			</div>
+
+				<div className="grid-con">
+					
+						{/* <Image cloudName="ajiva" className="img1" publicId="quikbuy/hotsneakers.jpg">
+							<Transformation quality="auto" fetchFormat="auto" />
+						</Image>
+						<Image cloudName="ajiva" className="img2" publicId="quikbuy/jeanjacket.jpg">
+							<Transformation quality="auto" fetchFormat="auto" />
+						</Image>
+						<Image cloudName="ajiva" className="img3" publicId="quikbuy/vans.jpg">
+							<Transformation quality="auto" fetchFormat="auto" />
+						</Image>
+						<Image cloudName="ajiva" className="img4" publicId="quikbuy/redbag.jpg">
+							<Transformation quality="auto" fetchFormat="auto" />
+						</Image> */}
+						<img className="img1" src={one} alt=""/>
+						<img className="img2" src={two} alt=""/>
+						<img className="img3" src={three} alt=""/>
+						<img className="img4" src={four} alt=""/>
+				</div>
 			</div>
 			<div className="hero-bg">
-				<img src="https://res.cloudinary.com/ajiva/image/upload/v1563881385/quikbuy/ladybg.jpg" alt="Img"/>
+				<Image cloudName="ajiva" publicId="quikbuy/ladybg.jpg">
+					<Transformation quality="auto" />
+				</Image>
 			</div>
 		</div>
-		// 1john 3:22-23
 	);
-}
-export default Hero
-	
-
+};
+export default Hero;
